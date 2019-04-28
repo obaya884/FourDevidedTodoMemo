ecc85b8eb08f2a07350575abd547b3a98333bbf5
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-04-28 17:29:30 +0900

    Initial Commit

---
+++ 4DevidedTodoMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4DevidedTodoMemo
+//
+//  Created by 大林拓実 on 2019/04/28.
+//  Copyright © 2019 TakumiObayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4DevidedTodoMemoTests/_DevidedTodoMemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  _DevidedTodoMemoTests.swift
+//  4DevidedTodoMemoTests
+//
+//  Created by 大林拓実 on 2019/04/28.
+//  Copyright © 2019 TakumiObayashi. All rights reserved.
+//
+
+import XCTest
+@testable import _DevidedTodoMemo
+
+class _DevidedTodoMemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
